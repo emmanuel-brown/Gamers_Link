426001f36240ffe69a1b4122d525280fd9174754
Emmanuel Oklo Brown <emmanuelbrown1310@gmail.com>
2020-01-13 11:21:38 -0500

    sovled mixing css issue

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,11 +2,12 @@ import React, {useState} from 'react'
 import Hero from '../../components/home/hero'
 import Content from '../../components/home/content'
 
+
 const Home = () =>(
-    <>
+    <Body>
         <Hero />
         <Content />
-    </>
+    </Body>
 )
 
 export default Home
