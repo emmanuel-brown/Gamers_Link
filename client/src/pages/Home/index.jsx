7fbd22f8e4a199008f05c45c0bfcb0e22ff418ce
Christiam Hernandez <christiamhernandez@Administrators-MacBook-Pro.local>
2020-01-13 11:20:54 -0500

    Added changes to background

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,16 @@
 import React, {useState} from 'react'
 import Hero from '../../components/home/hero'
 import Content from '../../components/home/content'
+import styled from "styled-components"
 
+const Body = styled.body`
+    background-color: #fff 
+`
 const Home = () =>(
-    <>
+    <Body>
         <Hero />
         <Content />
-    </>
+    </Body>
 )
 
 export default Home
